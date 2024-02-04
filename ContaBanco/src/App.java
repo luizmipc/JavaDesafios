81339522b4ce439f281d073f12a887585dec5e00
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-02-04 05:42:58 -0300

    Update App.java

--- ContaBanco/src/App.java
+++ ContaBanco/src/App.java
@@ -2,7 +2,12 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
-    static Scanner sc = new Scanner(System.in).useLocale(Locale.US);    
+    static Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+    /**
+    * <b>Método da classe App</b>
+    * <p>Define o número da conta bancária.</p>
+    */
     public static void informeNumero(ContaTerminal conta) {
 
         System.out.print("Por favor, digite o número da conta bancária! ");
@@ -10,6 +15,10 @@ public class App {
         System.out.printf("Número da conta: %d\n", conta.numero);
     }
 
+    /**
+    * <b>Método da classe App</b>
+    * <p>Define o número da agência.</p>
+    */
     public static void informeAgencia(ContaTerminal conta) {
 
         System.out.print("Por favor, digite o número da agência! ");
@@ -18,6 +27,10 @@ public class App {
 
     }
 
+    /**
+    * <b>Método da classe App</b>
+    * <p>Define o nome do cliente.</p>
+    */
     public static void informeNomeCliente(ContaTerminal conta) {
 
         System.out.print("Por favor, digite o nome do cliente! ");
@@ -26,6 +39,10 @@ public class App {
 
     }
 
+    /**
+    * <b>Método da classe App</b>
+    * <p>Define o saldo da conta bancária.</p>
+    */
     public static void informeSaldo(ContaTerminal conta) {
 
         System.out.print("Por favor, digite o saldo! ");
@@ -34,6 +51,10 @@ public class App {
 
     }
 
+    /**
+    * <b>Método da classe App</b>
+    * <p>Define os atributos do objeto: contaTerminal.</p>
+    */
     public static void cadastrarContaBancaria(ContaTerminal contaTerminal) {
         informeNomeCliente(contaTerminal);
         informeAgencia(contaTerminal);
@@ -41,6 +62,10 @@ public class App {
         informeSaldo(contaTerminal);
     }
     
+    /**
+    * <b>Método da classe App</b>
+    * <p>Exibe o nome do cliente, o número da conta, o número da conta bancária e o valor do saldo.</p>
+    */
     public static void exibirInformacoesCliente(ContaTerminal conta){
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
         conta.nomeCliente,
